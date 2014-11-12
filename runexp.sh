2c10285a754738c03464b07229b189074ef6762b
Lisa Z <lisaz@Lisas-MacBook-Air.local>
2014-11-11 23:06:43 -0500

    Tweaked inputs for experiments

--- runexp.sh
+++ runexp.sh
@@ -5,7 +5,7 @@ echo "Testing performance of msort with different file sizes"
 
 ./msort newschema.json meddata meddatamsort 1000 5 student_number
 
-#./msort newschema.json bigdata bigdatamsort 1000 5 student_number
+./msort newschema.json bigdata bigdatamsort 1000 5 student_number
 
 echo "Testing performance of msort with different memory capacities"
 
@@ -17,17 +17,19 @@ echo "Testing performance of msort with different memory capacities"
 
 ./msort newschema.json meddata bigmemmsort 22400 5 student_number
 
+./msort newschema.json meddata reallybigmemmsort 44800 5 student_number
+
 echo "Testing performance of msort with different values of k"
 
-./msort newschema.json meddata smallkmsort 1000 2 student_number 
+./msort newschema.json meddata smallkmsort 6000 2 student_number 
 
-./msort newschema.json meddata mid1kmsort 1000 5 student_number
+./msort newschema.json meddata mid1kmsort 6000 5 student_number
 
-./msort newschema.json meddata mid2kmsort 1000 10 student_number
+./msort newschema.json meddata mid2kmsort 6000 10 student_number
 
-./msort newschema.json meddata mid3kmsort 1000 20 student_number
+./msort newschema.json meddata mid3kmsort 6000 100 student_number
 
-./msort newschema.json meddata bigkmsort 5600 100 student_number
+./msort newschema.json meddata bigkmsort 60000 1000 student_number
 
 echo "Testing performance of bsort with different file sizes"
 
