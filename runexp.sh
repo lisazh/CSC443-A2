4dc41d7ed007e7beb2a88f095acd663c5d6641ec
Lisa Z <lisaz@Lisas-MacBook-Air.local>
2014-11-11 22:51:55 -0500

    Adding more things to experiments script

--- runexp.sh
+++ runexp.sh
@@ -16,3 +16,24 @@ echo "Testing performance of msort with different memory capacities"
 ./msort newschema.json meddata mid2memmsort 2800 5 student_number
 
 ./msort newschema.json meddata bigmemmsort 22400 5 student_number
+
+echo "Testing performance of msort with different values of k"
+
+./msort newschema.json meddata smallkmsort 1000 2 student_number 
+
+./msort newschema.json meddata mid1kmsort 1000 5 student_number
+
+./msort newschema.json meddata mid2kmsort 1000 10 student_number
+
+./msort newschema.json meddata mid3kmsort 1000 20 student_number
+
+./msort newschema.json meddata bigkmsort 5600 100 student_number
+
+echo "Testing performance of bsort with different file sizes"
+
+./bsort newschema.json smalldata smalldatabsort student_number
+
+./bsort newschema.json meddata meddatabsort student_number
+
+./bsort newschema.json bigdata bigdatabsort student_number
+
