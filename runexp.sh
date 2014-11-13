c7783d16c4bbfc3c9e5b71100c56888e8a02d82c
Lisa Z <lisaz@Lisas-MacBook-Air.local>
2014-11-13 00:07:26 -0500

    minor change to experiment script

--- runexp.sh
+++ runexp.sh
@@ -27,9 +27,9 @@ echo "Testing performance of msort with different values of k"
 
 ./msort newschema.json meddata mid2kmsort 6000 10 student_number
 
-./msort newschema.json meddata mid3kmsort 6000 100 student_number
+./msort newschema.json meddata mid3kmsort 6000 50 student_number
 
-./msort newschema.json meddata bigkmsort 60000 1000 student_number
+./msort newschema.json meddata bigkmsort 6000 100 student_number
 
 echo "Testing performance of bsort with different file sizes"
 
